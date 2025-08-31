4545c8603459bb85299b34daf2f6319ad71c0b66
RedSummit1 <jshieldsf@gmail.com>
2025-08-31 01:45:14 -0400

    Enforce singleton design pattern for Emma bot instance

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from emma import Emma
 from dotenv import dotenv_values
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+config = dotenv_values(".env")
+emma_bot = Emma(token=config["SLACK_BOT_TOKEN"])
+
 if __name__ == "__main__":
-    config = dotenv_values(".env")
-    SocketModeHandler(app=Emma(token=config["SLACK_BOT_TOKEN"]), app_token=config["SOCKET_MODEL_TOKEN"]).start()
+    SocketModeHandler(app=emma_bot, app_token=config["SOCKET_MODEL_TOKEN"]).start()
