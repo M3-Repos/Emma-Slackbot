ae7032cee952220ffa62032ec0f51c62dc3605f0
RedSummit1 <jshieldsf@gmail.com>
2025-08-31 23:18:01 -0400

    Clean raw slack input to mode

--- emma.py
+++ emma.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import json
+import re
 from slack_bolt import App
 from collections import deque
 
@@ -17,14 +18,15 @@ class Emma(App):
         @self.event("app_mention")
         def handle_app_mentions(body, say):
             """When someone @mentions the bot"""
-            text = body["event"]["text"]
+            raw_text = body["event"]["text"]
+            text = re.sub(r'<@\w+>',"",raw_text).strip()
             user_id = body["event"]["user"]  # Gets the user ID like "U1234567890"
             user_memory = Emma.user_memories.setdefault(user_id,deque(maxlen=10))
             user_memory.append({"role":"user","content":text})
             ai_response = self.ask_ollama(user_memory)
             user_memory.append({"role":"assistant","content":ai_response})
             #say(f"{ai_response}\nUser ID: {user_id}")
-            say(f"{user_memory}")
+            say(f"{ai_response}")
 
     def init_message(self):
         @self.message("hello")
